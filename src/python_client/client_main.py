8a856bd64df59d0fdc45389ca4762d6b6171ed8e
mohamad <rafieiyan.moh@gmail.com>
2022-11-03 19:16:15 +0330

    added some base functionality

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -4,10 +4,65 @@ from base import BaseAgent, Action
 
 class Agent(BaseAgent):
 
+    SCORE_DIAMOND = [
+        ('11', 50),
+        ('12', 200),
+        ('13', 100),
+        ('14', 0),
+        ('21', 100),
+        ('22', 50),
+        ('23', 200),
+        ('24', 100),
+        ('31', 50),
+        ('32', 100),
+        ('33', 50),
+        ('34', 200),
+        ('41', 250),
+        ('42', 59),
+        ('43', 100),
+        ('44', 50),
+    ]
+
+
     def do_turn(self) -> Action:
-        return random.choice(
-            [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.UP_LEFT,
-             Action.UP_RIGHT, Action.NOOP])
+        agent = self.get_agent()
+        print(agent)
+        print(self.get_near_diamonds(self.get_diamonds(),agent[0], agent[1]))   
+        return Action.DOWN
+
+    def get_agent(self):
+        for i in range(self.grid_height):
+            for j in range(self.grid_width):
+                if 'A' in self.grid[i][j]:
+                    return (i,j)
+
+    def get_diamonds(self):
+        diamonds = []
+        for i in range(self.grid_height):
+            for j in range(self.grid_width):
+                if ('1' in self.grid[i][j]) or ('2' in self.grid[i][j]) or ('3' in self.grid[i][j]) or ('4' in self.grid[i][j]):
+                    diamonds.append({self.grid[i][j]: (i,j)})
+        return diamonds
+
+    def get_near_diamonds(self, diamonds, x, y):
+        near_diamonds = []
+        for i in range(len(diamonds)):
+            if list(diamonds[i].values())[0][0] <= x+2 and list(diamonds[i].values())[0][0] >= x-2 and list(diamonds[i].values())[0][1] <= y+2 and list(diamonds[i].values())[0][1] >= y-2:
+                near_diamonds.append(diamonds[i])
+        return near_diamonds
+
+    def is_reachable(self, agent, diamond):
+        """check turn, score, walls"""
+        pass
+
+    def find_route(self, agent, diamond):
+        """return score"""
+        pass
+
+    def get_nearest_diamond(self, agent, diamonds):
+        pass
+
+    
 
 
 if __name__ == '__main__':

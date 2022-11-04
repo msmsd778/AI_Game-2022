07f2d2dac03db193bb7b56c66f20226472bb02a8
Masoud <msmsd778@gmail.com>
2022-11-04 19:46:27 +0330

    Added functions and global variables
    get_nearest_diamond function was implemented.
    get_barriers function was implemented.
    Added global variables for A* algorithm.
     find_route function was implemented incompletely.

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -23,11 +23,16 @@ class Agent(BaseAgent):
         ('44', 50),
     ]
 
+    openSet = []
+    closedSet = []
+    cost_grid = []
+    diamonds = dict()
+    walls = []
 
     def do_turn(self) -> Action:
-        agent = self.get_agent()
-        print(agent)
-        print(self.get_near_diamonds(self.get_diamonds(),agent[0], agent[1]))   
+        # agent = self.get_agent()
+        # print(agent)
+        # print(self.get_near_diamonds(self.get_diamonds(),agent[0], agent[1]))   
         return Action.DOWN
 
     def get_agent(self):
@@ -37,34 +42,91 @@ class Agent(BaseAgent):
                     return (i,j)
 
     def get_diamonds(self):
-        diamonds = []
         for i in range(self.grid_height):
             for j in range(self.grid_width):
                 if ('1' in self.grid[i][j]) or ('2' in self.grid[i][j]) or ('3' in self.grid[i][j]) or ('4' in self.grid[i][j]):
-                    diamonds.append({self.grid[i][j]: (i,j)})
-        return diamonds
-
+                    self.diamonds.append({self.grid[i][j]: (i,j)})
+    
     def get_near_diamonds(self, diamonds, x, y):
         near_diamonds = []
         for i in range(len(diamonds)):
             if list(diamonds[i].values())[0][0] <= x+2 and list(diamonds[i].values())[0][0] >= x-2 and list(diamonds[i].values())[0][1] <= y+2 and list(diamonds[i].values())[0][1] >= y-2:
                 near_diamonds.append(diamonds[i])
         return near_diamonds
+    
+    def get_barriers(self):
+        for i in range(self.grid_height):
+            for j in range(self.grid_width):
+                if 'W' in self.grid[i][j]:
+                    self.walls.append((i,j))
 
     def is_reachable(self, agent, diamond):
         """check turn, score, walls"""
-        pass
+        
 
     def find_route(self, agent, diamond):
         """return score"""
-        pass
+        start = (0,0)
+        end = (self.grid_height-1, self.grid_width-1)
+        self.openSet.append(start)
+        if len(self.openSet) > 0:
+            #keep going
+            winner = 0
+            for i in range(i, len(self.openSet)):
+                if self.openSet[i].f < self.openSet[winner].f:
+                    winner = i
+                    
+            current = self.openSet[winner]
+                    
+            if current == end:
+                return Action.NOOP
+            else:
+                closedSet.append(current)
+                # Done coding from A* video until Part 1 min 25
+            
+        else:
+            #no solution
+            pass
 
     def get_nearest_diamond(self, agent, diamonds):
-        pass
-
-    
+        #implemented with diagonal distance as the heuristic function
+        x = self.get_agent[0]
+        y = self.get_agent[1]
+        D = 1
+        D2 = 2
+        h = 2**32
+        for i in range(len(diamonds)):  
+            dx = abs(x - list(l[i].values())[0][0])
+            dy = abs(y - list(l[i].values())[0][1])
+            tmp = D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
+            if tmp < h:
+                h = tmp
+                coordinate = (list(l[i].values())[0][0],list(l[i].values())[0][1])
+        return coordinate
 
 
 if __name__ == '__main__':
     data = Agent().play()
     print("FINISH : ", data)
+
+
+# These are bullshit. I was trying to implement the a* in a seprate part but I regretted
+# Real shit is in "find route" function. Focus on that!
+
+# class AStarGraph(object):
+#     def __init__(self):
+#         self.barriers = Agent.get_barriers
+    
+#     def heuristic(self, start, goal):
+#         D = 1
+#         D2 = 2
+#         dx = abs(start[0] - goal[0])
+#         dy = abs(start[1] - goal[1])
+#         return D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
+    
+#     def get_vertex_neighbors(self, pos):
+#         n = []
+#         for dx, dy, in [(1, 0), (-1, 0), (0, -1), (1, 1)]:
+#             x2 = pos[0] + dx
+#             y2 = pos[1]+ dy
+#             if x2 < 0 or x2 > 5 or y2 > 5

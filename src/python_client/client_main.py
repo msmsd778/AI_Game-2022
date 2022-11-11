fc992643f5d8d06435ce4f0973d20a145a76fadf
Masoud <msmsd778@gmail.com>
2022-11-12 00:11:41 +0330

    added final stuff

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -2,7 +2,7 @@ import random
 from base import BaseAgent, Action
 from MainClass import Node
 from itertools import permutations
-
+import math
 
 class Agent(BaseAgent):
 
@@ -43,6 +43,7 @@ class Agent(BaseAgent):
     collected_diamonds = '0'
     collected_items = ''
     ignored = []
+    keys = []
     path = []
     dest = ()
     grid_nodes = list()
@@ -51,18 +52,18 @@ class Agent(BaseAgent):
     total_clusters = []
     clustering_ignored = []
     clustered = False
+    clusters_scores = []
+    cluster_centers = []
+    clusters_distance = []
+    x = []
+    ratios = []
 
     def do_turn(self) -> Action:
-        # self.count += 1
-        # if self.count == 1:
-        #     self.create_grid_nodes()
-        #     self.get_items()
         
         if len(self.direction) == 0:
             self.path = []
             self.grid_nodes = []
             self.diamonds = []
-            # self.ignored = []
             self.closedList = []
             self.openList = []
             diamond_type = ''
@@ -75,41 +76,35 @@ class Agent(BaseAgent):
                 self.clustering()
                 self.clustered = True
                 for i in self.total_clusters:
-                    print(self.cluster_max_score(i)) # to be saved somewhere
-            
-            
-            if len(self.diamonds) == 0:
-                return Action.NOOP
+                    self.clusters_scores.append(self.cluster_max_score(i))
+                    self.cluster_centers.append(self.find_center(i))
 
-            nd = self.get_near_diamonds(self.diamonds, agent[0], agent[1])
-            # print(self.ignored)
-            if len(nd) == 0:
-                self.dest = self.get_nearest_diamond(self.diamonds)
-                for i in self.diamonds:
-                    if list(i.values())[0] == self.dest:
-                        diamond_type = list(i.keys())[0]
+            if len(self.clusters_scores) > 0:
+                self.ratios = self.distance_to_center()
+
+            if len(self.diamonds) == 0:
+                pass
+
+            
+            
+            if len(self.x) == 0 and len(self.ratios) != 0:  
+                idx = self.ratios.index(max(self.ratios)) # find the index of max ratio
+                self.x = self.clusters_scores.pop(idx)[0]
+                self.cluster_centers.pop(idx)
+                self.ratios.pop(idx)
+            if len(self.x) > 0:
+                i = self.x.pop(0)
+                self.dest = i[1]            
+                found = self.A_star(agent, self.dest)
             else:
-                if len(nd) > 1:
-                    tmp_dict = dict()
-                    for i in range(len(nd)):
-                        tmp_dict[list(nd[i].values())[0]] = self.heuristic(self.get_agent(), list(nd[i].values())[0])
-                        
-                    tmp_dict = dict(sorted(tmp_dict.items(), key=lambda item: item[1]))
-                    self.dest = list(tmp_dict)[0]
-                    for i in range(len(nd)):
-                        if list(nd[i].values())[0] == self.dest:
-                            diamond_type = list(nd[i].keys())[0]
+                if len(self.keys) > 0:
+                    self.dest = self.keys.pop(0)
+                    found = self.A_star(agent, self.dest)
                 else:
-                    self.dest = list(nd[0].values())[0] # calculate sequence later!
-                    diamond_type = list(nd[0].keys())[0]
-                self.collected_items = self.collected_items + diamond_type   #not here: diamond might not be accessible
-            found = self.A_star(agent, self.dest)
+                    return Action.NOOP
             if not found and self.dest not in self.ignored:
-                # print(self.dest)
                 self.ignored.append(self.dest)
 
-
-        
         try:
             x = self.direction.pop(0)
             return x
@@ -138,25 +133,22 @@ class Agent(BaseAgent):
         return near_diamonds
 
 
-    def get_nearest_diamond(self, diamonds):
+    def get_nearest_key(self, keys):
         '''implemented with diagonal distance as the heuristic function'''
         x = self.get_agent()[0]
         y = self.get_agent()[1]
         D = 1
         D2 = 2
         h = 2**32
-        diamond_type = ''
-        coordinate = list(diamonds[0].values())[0]
-        for i in range(len(diamonds)):  
-            dx = abs(x - list(diamonds[i].values())[0][0])
-            dy = abs(y - list(diamonds[i].values())[0][1])
+        coordinate = list(keys[0].values())[0]
+        for i in range(len(keys)):  
+            dx = abs(x - list(keys[i].values())[0][0])
+            dy = abs(y - list(keys[i].values())[0][1])
             tmp = D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
             if tmp < h:
                 h = tmp
-                coordinate = list(diamonds[i].values())[0]
-                diamond_type = list(diamonds[i].keys())[0]
+                coordinate = list(keys[i].values())[0]
                 
-        self.collected_items = self.collected_items + diamond_type
         return coordinate
     
     
@@ -173,7 +165,7 @@ class Agent(BaseAgent):
             dy = abs(y - diamonds[i][1])
             tmp = D * (dx + dy) + (D2 - 2 * D) * min(dx, dy)
             if not initialization: 
-                if tmp < h and tmp <= 3:
+                if tmp < h and tmp <= 2:
                     h = tmp
                     coordinate = diamonds[i]
             else:
@@ -214,28 +206,50 @@ class Agent(BaseAgent):
     
     def cluster_max_score(self, cords_list):
         sequence = []
-        for i in cords_list:
+        for i in cords_list:                                # find the diamond type of each coordinate
             for j in range(len(self.diamonds)):
                 if list(self.diamonds[j].values())[0] == i:
-                        sequence.append(list(self.diamonds[j].keys())[0])
+                        sequence.append([list(self.diamonds[j].keys())[0], list(self.diamonds[j].values())[0]])
                 
         perm = list(permutations(sequence))
         my_max = 0
         selected_perm = []
         for j in perm:
+            cord_seq = []
             s = 0
             for i in range(len(j) - 1):
-                if str(j[i]) + str(j[i+1]) in list(self.DIAMOND_SCORES.keys()):
-                    s = s + self.DIAMOND_SCORES[str(j[i]) + str(j[i+1])]
+                if str(j[i][0]) + str(j[i+1][0]) in list(self.DIAMOND_SCORES.keys()):
+                    s = s + self.DIAMOND_SCORES[str(j[i][0]) + str(j[i+1][0])]
+                    cord_seq.append(j[i][1])
             if s >= my_max:
                 my_max = s
                 selected_perm = j
                 
-        return selected_perm, my_max
+        return [list(selected_perm), my_max]
 
     
+    def find_center(self, cords):
+        x = y = 0
+        for i in cords:
+                x = x + i[0]
+                y = y + i[1]
+        x = x / len(cords)
+        y = y / len(cords)
+        return [x, y]
     
-            
+    
+    
+    def distance_to_center(self):
+        distances = []
+        ratios = []
+        for i in self.cluster_centers:
+            distances.append(math.dist(self.get_agent(), i))
+        for i in range(len(distances)):
+            if distances[i] == 0:
+                continue
+            ratios.append(self.clusters_scores[i][1] / distances[i])
+        return ratios
+    
     
     def create_grid_nodes(self):
         self.grid_nodes = [None] * self.grid_height
@@ -247,15 +261,6 @@ class Agent(BaseAgent):
                 self.grid_nodes[i][j] = Node((i,j), 0, 0)
                 
     
-    # def get_keys(self):
-    #     for i in range(self.grid_height):
-    #         for j in range(self.grid_width):
-    #             if 'g' in self.grid[i][j]:
-    #                 self.grid_nodes[i][j].key = 'g'
-    #             elif 'r' in self.grid[i][j]:
-    #                 self.grid_nodes[i][j].key = 'r'
-    #             elif 'y' in self.grid[i][j]:
-    #                 self.grid_nodes[i][j].key = 'y'
     
     def get_items(self):
         for i in range(self.grid_height):
@@ -263,7 +268,7 @@ class Agent(BaseAgent):
                 if 'W' in self.grid[i][j]:
                     self.grid_nodes[i][j].is_wall = True
                 elif '*' in self.grid[i][j]:
-                    self.grid_nodes[i][j].wired = True
+                    self.grid_nodes[i][j].is_wired = True
                 elif 'G' in self.grid[i][j] and 'G' not in self.collected_keys:
                     self.grid_nodes[i][j].door = 'G'
                     self.grid_nodes[i][j].is_door = True
@@ -275,9 +280,12 @@ class Agent(BaseAgent):
                     self.grid_nodes[i][j].is_door = True
                 elif 'g' in self.grid[i][j]:
                     self.grid_nodes[i][j].key = 'g'
+                    self.keys.append((i,j))
                 elif 'r' in self.grid[i][j]:
                     self.grid_nodes[i][j].key = 'r'
+                    self.keys.append((i,j))
                 elif 'y' in self.grid[i][j]:
+                    self.keys.append((i,j))
                     self.grid_nodes[i][j].key = 'y'
 
     
@@ -313,10 +321,13 @@ class Agent(BaseAgent):
                 self.path = []
                 temp = currentNode
                 self.path.append(temp)
+                self.check_key(temp)
                 while(temp.previous):
                     self.check_key(temp.previous)
                     self.path.append(temp.previous)
                     temp = temp.previous
+                for node in self.path:
+                    print(node.cords)
 
                 self.find_direction()
                 break
@@ -400,163 +411,15 @@ class Agent(BaseAgent):
         if node.key and node.key.upper() not in self.collected_keys:
             self.collected_keys.append(node.key.upper())
             self.ignored = []
-            # for i in range(self.grid_height):
-            #     for j in range(self.grid_width):
-            #         if self.grid_nodes[i][j].door == node.key.upper():
-            #             self.grid_nodes[i][j].door = ''
-            #             self.grid_nodes[i][j].is_door = False
+            self.cluster_centers = []
+            self.clustering_ignored = []
+            self.clusters_scores = []
+            self.clusters_distance = []
+            self.total_clusters = []
+            self.clustered = False
 
 
 if __name__ == '__main__':
     data = Agent().play()
     print("FINISH : ", data)
     
-# import math
-# import random
-# import matplotlib.pyplot as plt
-
-# #First function to optimize
-# # def heuristic(x):
-# #     value = -x**2
-# #     return value
-
-# #Second function to optimize
-# def function2(x):
-#     value = -(x-2)**2
-#     return value
-
-# #Function to find index of list
-# def index_of(a,list):
-#     for i in range(0,len(list)):
-#         if list[i] == a:
-#             return i
-#     return -1
-
-# #Function to sort by values
-# def sort_by_values(list1, values):
-#     sorted_list = []
-#     while(len(sorted_list)!=len(list1)):
-#         if index_of(min(values),values) in list1:
-#             sorted_list.append(index_of(min(values),values))
-#         values[index_of(min(values),values)] = math.inf
-#     return sorted_list
-
-# #Function to carry out NSGA-II's fast non dominated sort
-# def fast_non_dominated_sort(values1, values2):
-#     S=[[] for i in range(0,len(values1))]
-#     front = [[]]
-#     n=[0 for i in range(0,len(values1))]
-#     rank = [0 for i in range(0, len(values1))]
-
-#     for p in range(0,len(values1)):
-#         S[p]=[]
-#         n[p]=0
-#         for q in range(0, len(values1)):
-#             if (values1[p] > values1[q] and values2[p] > values2[q]) or (values1[p] >= values1[q] and values2[p] > values2[q]) or (values1[p] > values1[q] and values2[p] >= values2[q]):
-#                 if q not in S[p]:
-#                     S[p].append(q)
-#             elif (values1[q] > values1[p] and values2[q] > values2[p]) or (values1[q] >= values1[p] and values2[q] > values2[p]) or (values1[q] > values1[p] and values2[q] >= values2[p]):
-#                 n[p] = n[p] + 1
-#         if n[p]==0:
-#             rank[p] = 0
-#             if p not in front[0]:
-#                 front[0].append(p)
-
-#     i = 0
-#     while(front[i] != []):
-#         Q=[]
-#         for p in front[i]:
-#             for q in S[p]:
-#                 n[q] =n[q] - 1
-#                 if( n[q]==0):
-#                     rank[q]=i+1
-#                     if q not in Q:
-#                         Q.append(q)
-#         i = i+1
-#         front.append(Q)
-
-#     del front[len(front)-1]
-#     return front
-
-# #Function to calculate crowding distance
-# def crowding_distance(values1, values2, front):
-#     distance = [0 for i in range(0,len(front))]
-#     sorted1 = sort_by_values(front, values1[:])
-#     sorted2 = sort_by_values(front, values2[:])
-#     distance[0] = 4444444444444444
-#     distance[len(front) - 1] = 4444444444444444
-#     for k in range(1,len(front)-1):
-#         distance[k] = distance[k]+ (values1[sorted1[k+1]] - values2[sorted1[k-1]])/(max(values1)-min(values1))
-#     for k in range(1,len(front)-1):
-#         distance[k] = distance[k]+ (values1[sorted2[k+1]] - values2[sorted2[k-1]])/(max(values2)-min(values2))
-#     return distance
-
-# #Function to carry out the crossover
-# def crossover(a,b):
-#     r=random.random()
-#     if r>0.5:
-#         return mutation((a+b)/2)
-#     else:
-#         return mutation((a-b)/2)
-
-# #Function to carry out the mutation operator
-# def mutation(solution):
-#     mutation_prob = random.random()
-#     if mutation_prob <1:
-#         solution = min_x+(max_x-min_x)*random.random()
-#     return solution
-
-# #Main program starts here
-# pop_size = 20
-# max_gen = 921
-
-# #Initialization
-# min_x=-55
-# max_x=55
-# solution=[min_x+(max_x-min_x)*random.random() for i in range(0,pop_size)]
-# gen_no=0
-# while(gen_no<max_gen):
-#     heuristic_values = [heuristic(solution[i])for i in range(0,pop_size)]
-#     function2_values = [function2(solution[i])for i in range(0,pop_size)]
-#     non_dominated_sorted_solution = fast_non_dominated_sort(heuristic_values[:],function2_values[:])
-#     print("The best front for Generation number ",gen_no, " is")
-#     for valuez in non_dominated_sorted_solution[0]:
-#         print(round(solution[valuez],3),end=" ")
-#     print("\n")
-#     crowding_distance_values=[]
-#     for i in range(0,len(non_dominated_sorted_solution)):
-#         crowding_distance_values.append(crowding_distance(heuristic_values[:],function2_values[:],non_dominated_sorted_solution[i][:]))
-#     solution2 = solution[:]
-#     #Generating offsprings
-#     while(len(solution2)!=2*pop_size):
-#         a1 = random.randint(0,pop_size-1)
-#         b1 = random.randint(0,pop_size-1)
-#         solution2.append(crossover(solution[a1],solution[b1]))
-#     heuristic_values2 = [heuristic(solution2[i])for i in range(0,2*pop_size)]
-#     function2_values2 = [function2(solution2[i])for i in range(0,2*pop_size)]
-#     non_dominated_sorted_solution2 = fast_non_dominated_sort(heuristic_values2[:],function2_values2[:])
-#     crowding_distance_values2=[]
-#     for i in range(0,len(non_dominated_sorted_solution2)):
-#         crowding_distance_values2.append(crowding_distance(heuristic_values2[:],function2_values2[:],non_dominated_sorted_solution2[i][:]))
-#     new_solution= []
-#     for i in range(0,len(non_dominated_sorted_solution2)):
-#         non_dominated_sorted_solution2_1 = [index_of(non_dominated_sorted_solution2[i][j],non_dominated_sorted_solution2[i] ) for j in range(0,len(non_dominated_sorted_solution2[i]))]
-#         front22 = sort_by_values(non_dominated_sorted_solution2_1[:], crowding_distance_values2[i][:])
-#         front = [non_dominated_sorted_solution2[i][front22[j]] for j in range(0,len(non_dominated_sorted_solution2[i]))]
-#         front.reverse()
-#         for value in front:
-#             new_solution.append(value)
-#             if(len(new_solution)==pop_size):
-#                 break
-#         if (len(new_solution) == pop_size):
-#             break
-#     solution = [solution2[i] for i in new_solution]
-#     gen_no = gen_no + 1
-
-# #Lets plot the final front now
-# heuristic = [i * -1 for i in heuristic_values]
-# function2 = [j * -1 for j in function2_values]
-# plt.xlabel('Function 1', fontsize=15)
-# plt.ylabel('Function 2', fontsize=15)
-# plt.scatter(heuristic, function2)
-# plt.show()
